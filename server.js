0e96785350892aeac9b6bc8a21d59d5ad7723c2e
kartik0603 <kartikhirapara800@gmail.com>
2024-11-27 01:00:06 +0530

    Library Management-App Backend

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 
   connectDB()
